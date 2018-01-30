e30965b49791ef3c21e6c5fa15563f5e7fca06d6
Your Name <you@example.com>
2018-01-30 10:58:37 +0800

    添加代码

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// const css = require('./app.css'); // 要包含的css文件
+const css = require('./app.scss'); 
+console.log("aabdddaaacccaaa");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<h1>Hello ,World!</h1>,
+	document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require("webpack");
+module.exports = {
+    // entry: './src/app.js',
+    entry: {
+    	app:'./src/app.js',
+    	contact:'./src/contact.js'
+    },
+    /*output:{
+    	filename:'./dist/app.bundle.js'
+    },*/
+    output: {
+        // path: 'C:/Users/lpz/webpacktest/dist/',
+        path: path.resolve(__dirname, './dist'),
+        // filename: 'app.bundle.js'
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            // {test: /\.scss$/,use:['style-loader','css-loader','sass-loader']}, //指定文件后缀用css-loader
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    loader: ['css-loader', 'sass-loader'],
+                    publicPath: '/dist'
+                })
+            } ,
+            //指定文件后缀用css-loader
+            //{test: /\.scss$/,loaders:'style-loader!css-loader'} //指定文件后缀用css-loader
+            {
+            	test: /\.js$/,
+            	exclude: /node_module/,
+            	use: 'babel-loader'
+            },
+            {
+            	test: /\.pug$/,
+            	use: ['html-loader','pug-html-loader']
+            }
+        ]
+    },
+    devServer:{
+    	contentBase:path.join(__dirname,"dist"),
+    	compress:true,
+    	port:3000,
+    	hot:true,
+    	stats:"errors-only",
+    	open:true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '网页模版2',
+            hash: true, // 生成js版本编号
+            excludeChunks:['contact'],
+            // filename: './../index.html',
+            //template:'src/index.ejs' //ejs
+            template: 'src/index.pug' //html
+        }),
+        new HtmlWebpackPlugin({
+            title: '联系人模版2',
+            minify: {
+                collapseWhitespace: true // 折叠生成的html代码
+            },
+            hash: true, // 生成js版本编号
+            include: ['contact'],
+            filename: 'contact.html', // 指定生成的文件名
+            template: 'src/contact.html' //html
+        }),
+        new ExtractTextPlugin({
+        	filename:'app.css',
+        	disable:false,
+        	allChunks:true
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+
+    ]
+}
